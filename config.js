38de8188491daf80e621c176bad33b2094219deb
Mrlion <contact@lionsproject.eu>
2023-10-14 19:41:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
-dotenv.config({ path: `${process.cwd()}/.${process.env.NODE_ENV.replace(' ', '')}.env`});
+dotenv.config({ path: `${process.cwd()}/.${process.env.NODE_ENV.replace(' ', '')}.env` });
 
 module.exports = {
     token: process.env.DISCORD_TOKEN,
-    prefix: process.env.PREFIX, 
-    ownerID: process.env.OWNER_ID, // Mrlion (710549603216261141)
+    prefix: process.env.PREFIX,
+    ownerID: process.env.OWNER_ID,
 };
+
+require('./status.js');
+require('./reload.js');
