9f3227efa82d081b8ecb23626973a85d2d06fce2
Mrlion <contact@lionsproject.eu>
2023-10-14 20:18:40 +0200

    Update counting.js

--- modules/counting.js
+++ modules/counting.js
@@ -1,26 +1,37 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const config = require("./config.js");
+require("dotenv").config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
-const COUNTING_CHANNEL = "process.env.COUNTING_CHANNEL"; 
+const COUNTING_CHANNEL = process.env.COUNTING_CHANNEL;
+const reactionEmoji = process.env.COUNTING_REACTION;
 
-let expectedNumber = 1; 
-const reactionEmoji = "process.env.COUNTING_REACTION"
+let expectedNumber = 1;
 
 client.on("messageCreate", async (message) => {
-    if (message.author.bot) return; 
+    if (message.author.bot) return;
 
-    if (message.channel.id !== COUNTING_CHANNEL) return; 
+    if (message.channel.id !== COUNTING_CHANNEL) return;
 
     const content = message.content;
 
-    if (parseInt(content) === expectedNumber) {
+    if (isNumeric(content) && parseInt(content) === expectedNumber) {
         expectedNumber++;
         message.react(reactionEmoji);
     } else {
-        message.delete();
+        if (content.toLowerCase() === "undo" && message.deletable) {
+            const lastNumber = expectedNumber - 1;
+            message.delete();
+            message.channel.send(`Poslední číslo bylo smazáno, číslo bylo **${lastNumber}**.`);
+        } else {
+            message.delete();
+        }
     }
 });
+
+function isNumeric(value) {
+    return !isNaN(value) && isFinite(value);
+}
 
 client.login(config.token);
