2c3c09601ac86e5629ca2c2418a548278cf1bf13
Mrlion <contact@lionsproject.eu>
2023-10-14 08:43:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const dotenv = require(dotenv);
+const dotenv = require('dotenv');
 
 dotenv.config({ path: `${process.cwd()}/.${process.env.NODE_ENV.replace(' ', '')}.env`});
 
 module.exports = {
-    token: "process.env.DISCORD_TOKEN",
-    prefix: "process.env.PREFIX",
-    ownerID: "process.env.OWNER_ID" // Mrlion (710549603216261141)
+    token: process.env.DISCORD_TOKEN,
+    prefix: process.env.PREFIX, 
+    ownerID: process.env.OWNER_ID, // Mrlion (710549603216261141)
 };
