f328ab8fed6453e8ec82a8f5357ad67835362d96
Mrlion <contact@lionsproject.eu>
2023-10-14 08:48:49 +0200

    Update status.js

--- status.js
+++ status.js
@@ -1,1 +1,16 @@
+const { Client } = require('discord.js');
+const config = require('./config.js');
 
+const client = new Client();
+
+client.once('ready', () => {
+    client.user.setPresence({
+        status: 'process.env.PRESENCE_STATUS', 
+        activity: {
+            name: 'process.env.PRESENCE_NAME',
+            type: 'process.env.PRESENCE_TYPE',
+        },
+    });
+});
+
+client.login(config.token);
