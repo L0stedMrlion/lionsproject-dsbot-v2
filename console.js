e48c33aefd74a1051eb55c24b32dce9aac5cc5d9
Mrlion <contact@lionsproject.eu>
2023-10-14 20:28:02 +0200

    Update console.js

--- console.js
+++ console.js
@@ -1,15 +1,34 @@
 const fs = require('fs');
 
+// Nastavení logovacího souboru
+const logFile = "logs/startuplog.log";
+
 console.log("==================================");
 console.log("   Discord Bot Initialization   ");
 console.log("==================================");
 
+// Funkce pro logování zpráv do souboru
+function logToConsoleAndFile(message) {
+    // Výpis zprávy do konzole
+    console.log(message);
+
+    // Zápis zprávy do logovacího souboru
+    fs.appendFile(logFile, message + "\n", (err) => {
+        if (err) {
+            console.error("Chyba při zápisu do logu:", err);
+        }
+    });
+}
+
 console.log("Loading files and modules...");
+logToConsoleAndFile("Loading files and modules...");
 
 function loadFilesFromDirectory(directoryPath) {
     fs.readdir(directoryPath, (err, files) => {
         if (err) {
-            console.error(`Error reading directory: ${err}`);
+            const errorMessage = `Error reading directory: ${err}`;
+            console.error(errorMessage);
+            logToConsoleAndFile(errorMessage);
             return;
         }
 
@@ -17,22 +36,32 @@ function loadFilesFromDirectory(directoryPath) {
             const filePath = `${directoryPath}/${file}`;
             try {
                 require(filePath);
-                console.log(`Loaded and executed file: ${file}`);
+                const successMessage = `Loaded and executed file: ${file}`;
+                console.log(successMessage);
+                logToConsoleAndFile(successMessage);
             } catch (e) {
-                console.error(`Error loading and executing file ${file}: ${e}`);
+                const errorMessage = `Error loading and executing file ${file}: ${e}`;
+                console.error(errorMessage);
+                logToConsoleAndFile(errorMessage);
             }
         });
 
-        console.log(`Files loaded from directory: ${directoryPath}`);
+        const filesLoadedMessage = `Files loaded from directory: ${directoryPath}`;
+        console.log(filesLoadedMessage);
+        logToConsoleAndFile(filesLoadedMessage);
     });
 }
 
 console.log("Loading commands from 'commands' directory...");
+logToConsoleAndFile("Loading commands from 'commands' directory...");
 loadFilesFromDirectory('./commands');
 
 console.log("Loading modules from 'modules' directory...");
+logToConsoleAndFile("Loading modules from 'modules' directory...");
 loadFilesFromDirectory('./modules');
 
 console.log("Additional initialization...");
+logToConsoleAndFile("Additional initialization...");
 
 console.log("Initialization completed.");
+logToConsoleAndFile("Initialization completed.");
