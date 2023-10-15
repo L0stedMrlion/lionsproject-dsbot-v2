9516d057a0984e02573ac739a86cc412d54a3162
Mrlion <contact@lionsproject.eu>
2023-10-15 10:57:37 +0200

    Simplier console a log system.

--- console.js
+++ console.js
@@ -2,54 +2,31 @@ const fs = require('fs');
 const currentDate = new Date();
 const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
 const formattedTime = `${currentDate.getHours()}:${currentDate.getMinutes()}`;
-const logFileName = `logs/${formattedDate}_${formattedTime}.log`;
+const logFileName = `logs/log_${formattedDate}_${formattedTime}.log`;
 
-function logToConsoleAndFile(message) {
-    console.log(message);
-    fs.appendFile(logFileName, message + "\n", (err) => {
-        if (err) {
-            console.error("Chyba při zápisu do logu:", err);
-        }
-    });
-}
+fs.writeFile(logFileName, '', (err) => {
+    if (err) {
+        console.error(`Failed to create a startup log ${err}`);
+    }
+});
 
-logToConsoleAndFile("Loading files and modules...");
+const logStream = fs.createWriteStream(logFileName, { flags: 'a' });
+console.log = function(message) {
+    logStream.write(message + '\n');
+    process.stdout.write(message + '\n');
+};
 
-function loadFilesFromDirectory(directoryPath) {
-    fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-            const errorMessage = `Error reading directory: ${err}`;
-            console.error(errorMessage);
-            logToConsoleAndFile(errorMessage);
-            return;
-        }
-
-        files.forEach((file) => {
-            const filePath = `${directoryPath}/${file}`;
-            try {
-                require(filePath);
-                const successMessage = `Loaded and executed file: ${file}`;
-                console.log(successMessage);
-                logToConsoleAndFile(successMessage);
-            } catch (e) {
-                const errorMessage = `Error loading and executing file ${file}: ${e}`;
-                console.error(errorMessage);
-                logToConsoleAndFile(errorMessage);
-            }
-        });
-
-        const filesLoadedMessage = `Files loaded from directory: ${directoryPath}`;
-        console.log(filesLoadedMessage);
-        logToConsoleAndFile(filesLoadedMessage);
-    });
-}
-
-logToConsoleAndFile("Loading commands from 'commands' directory...");
-loadFilesFromDirectory('./commands');
-
-logToConsoleAndFile("Loading modules from 'modules' directory...");
-loadFilesFromDirectory('./modules');
-
-logToConsoleAndFile("Additional initialization...");
-
-logToConsoleAndFile("Initialization completed.");
+// Console log
+console.log(" Lion's Project™ - Discord bot ", process.env.DSBOTVERSION)
+setTimeout(() => {
+    console.log(" Lion's Project™ - Loading...");
+}, 3000);
+setTimeout(() => {
+    console.log(" Lion's Project™ - Loading modules...");
+}, 6000);
+setTimeout(() => {
+    console.log(" Lion's Project™ - Loading commands...");
+}, 9000);
+client.on('ready', () => {
+    console.log(`Lion's Project™ - Bot is successfully enabled!`);
+});
