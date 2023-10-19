7bd5e429bf3e8fa60057e19d102a1b6c2e14e38b
Mrlion <contact@lionsproject.eu>
2023-10-19 17:46:50 +0000

    upd

--- src/commands.js
+++ src/commands.js
@@ -12,8 +12,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('Registering slash commands...');
-
     await rest.put(
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
