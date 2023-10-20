885fd958fa2de05a0cae1edf2b89c25f8f7e21f3
Mrlion <contact@lionsproject.eu>
2023-10-20 11:49:52 +0000

    moved to purge.js from index.js

---
+++ src/commands/purge.js
@@ -0,0 +1,9 @@
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'purge') {
+      const channel = interaction.channel;
+      channel.bulkDelete(100)
+      interaction.reply(":broom: Zprávy byly promazány!")
+    }
+  });
