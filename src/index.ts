dd9c7fed0264c3159f3a97977e55cf077ab9830b
Mrlion <contact@lionsproject.eu>
2023-10-21 11:43:43 +0000

    added new files, not tested and not working, will fix later ok?

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { config } from "dotenv";
-import { Client, GatewayIntentBits, ActivityType } from "discord.js";
-
+import { Client, GatewayIntentBits, ActivityType, Collection, Events } from "discord.js";
 config();
 
 const client = new Client({
@@ -44,5 +43,21 @@ client.on("ready", () => {
     });
   }
 });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 client.login(process.env.TOKEN);
