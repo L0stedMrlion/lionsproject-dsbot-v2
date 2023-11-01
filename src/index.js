36aa24c2ea3a2aff83a4d33510300a1830f0b1ed
Mrlion <contact@lionsproject.eu>
2023-11-01 17:21:11 +0000

    deleted some intents, formatted

--- src/index.js
+++ src/index.js
@@ -2,42 +2,42 @@ require('dotenv').config();
 const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildEmojisAndStickers,
-        GatewayIntentBits.GuildIntegrations,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildModeration,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildScheduledEvents,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildWebhooks,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.AutoModerationConfiguration,
-        GatewayIntentBits.AutoModerationExecution,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+  ],
 });
 
 client.once("ready", () => {
+  console.log(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
+  console.log(" 🦁 Lion's Project™ - Discord bot", process.env.BOT_VERSION);
+  console.log(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
+});
+
+client.on("ready", () => {
+  if (client.user) {
+    console.log(" 🦁 Lion's Project™ - Loading... ");
+    console.log(" 🦁 Lion's Project™ - Loaded! ");
+    console.log(" 🦁 Lion's Project™ - Bot have been successfully enabled! ");
     console.log(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
-    console.log(" 🦁 Lion's Project™ - Discord bot", process.env.BOT_VERSION);
-    console.log(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
-  });
-  
-  client.on("ready", () => {
-    if (client.user) {
-      console.log(" 🦁 Lion's Project™ - Loading... ");
-      console.log(" 🦁 Lion's Project™ - Loaded! ");
-      console.log(" 🦁 Lion's Project™ - Bot have been successfully enabled! ");
-      console.log(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
-      client.user.setActivity("🦁 Lion's Project", {
-        type: ActivityType.Listening,
-      });
-    }
-  });
+    client.user.setActivity("🦁 Lion's Project", {
+      type: ActivityType.Listening,
+    });
+  }
+});
 
 client.login(process.env.TOKEN);
