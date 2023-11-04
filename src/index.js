058321895418b3925ee6b44fd47a49617d1405d0
Mrlion <contact@lionsproject.eu>
2023-11-04 19:40:39 +0000

    edited discord server emoji

--- src/index.js
+++ src/index.js
@@ -51,10 +51,9 @@ client.on('interactionCreate', async (interaction) => {
         Hello, I'm LionBot! 🦁
 
         🔧  Developer: **${process.env.AUTHOR}**
-
         📋  Version: **${process.env.BOT_VERSION}**
+        📻  Discord server: [Discord](https://discord.gg/BYHTyMCJkh)
 
-        🎧  Discord server: [Discord](https://discord.gg/BYHTyMCJkh)
       `)
       .setColor('#ffbc00')
       .setThumbnail('https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3')
