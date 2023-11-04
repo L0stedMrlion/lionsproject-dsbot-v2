7a3a10beeb4d46f8977dca08c97e68c20acf9b5c
Mrlion <contact@lionsproject.eu>
2023-11-04 19:44:19 +0000

    test

--- src/index.js
+++ src/index.js
@@ -54,6 +54,8 @@ client.on('interactionCreate', async (interaction) => {
         📋  Version **${process.env.BOT_VERSION}**
         📻  Discord server [Discord](https://discord.gg/BYHTyMCJkh)
 
+        ${client.guilds.size}
+
       `)
       .setColor('#ffbc00')
       .setThumbnail('https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3')
