ca2e0a42c72294132289c42291c985b5824f62e9
Mrlion <contact@lionsproject.eu>
2023-11-05 09:42:49 +0000

    command register is now loaded on start, new command will be registered immidiatly

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, ActivityType, EmbedBuilder } = require("discord.js");
-
-// Jokes for (/joke)
-const jokes = [
-  "Why do Czechs always drink beer? Because it's the only way to wash down the cabbage.",
-  "What do you call a Czech who doesn't drink beer? A disappointment.",
-  'Whats the Czech word for "beer" ? "Pivo".Whats the English word for "beer" ? "Pivo".Whats the Czech word for "more beer" ? "Ještě pivo".',
-  "Why are Czechs so bad at driving? Because they're always looking for the nearest bar.",
-  'Whats the Czech word for "traffic jam"? "A line of cars waiting to get to the bar',
-  "Why are Czechs so good at chess? Because they're always planning their next beer.",
-  "Why are Czechs so good at skiing? Because they're always trying to get away from their problems.",
-  'Whats the Czech word for "money"? "Beer tokens".',
-  "Why are Czechs so good at gambling? Because they're always willing to risk everything for a beer.",
-  'Whats the Czech word for "everything"? "My last beer".',
-  'Whats the Czech word for "attention" ? "Free beer".',
-  'Whats the Czech word for "friend"? "Someone who will buy me a beer".',
-];
+require('./commands/commandRegister.js');
 
 const client = new Client({
   intents: [
@@ -105,24 +90,6 @@ client.on('interactionCreate', async (interaction) => {
   }
 });
 
-// Command (/joke)
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'joke') {
-    const joke = jokes[Math.floor(Math.random() * jokes.length)];
-    const embed = new EmbedBuilder();
-    embed.setTitle(`🐸 Joke of the day`);
-    embed.setDescription(joke);
-    embed.setColor('#1d6340');
-    embed.setFooter({
-      text: "🦁 Lion's Project™ © 2023",
-      iconURL: 'https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3',
-    });
-
-    interaction.reply({ embeds: [embed] });
-  }
-});
 
 try {
   client.login(process.env.TOKEN);
