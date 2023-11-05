b995ba668b7301057ca5dbe28a3c75fb2d35f5c5
Mrlion <contact@lionsproject.eu>
2023-11-05 08:04:06 +0000

    added footer

--- src/index.js
+++ src/index.js
@@ -106,7 +106,11 @@ client.on('interactionCreate', async (interaction) => {
     const embed = new EmbedBuilder();
     embed.setTitle(`Joke of the day`);
     embed.setDescription(joke);
-    embed.setColor('Random');
+    embed.setColor('#1d6340');
+    embed.setFooter({
+      text: "🦁 Lion's Project™ © 2023",
+      iconURL: 'https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3',
+    });
 
     interaction.reply({ embeds: [embed] });
   }
