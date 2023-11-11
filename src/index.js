18713ff7a5eb7ae5caff0e8c06d4747e538cc88f
Mrlion <contact@lionsproject.eu>
2023-11-11 14:20:49 +0000

    changed embed name for better orientation

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ client.on('interactionCreate', async (interaction) => {
 
   if (interaction.commandName === 'joke') {
     const joke = jokes[Math.floor(Math.random() * jokes.length)];
-    const embed = new EmbedBuilder();
+    const joke_embd = new EmbedBuilder();
     embed.setTitle(`🐸 Joke of the day`);
     embed.setDescription(joke);
     embed.setColor('#1d6340');
@@ -98,7 +98,7 @@ client.on('interactionCreate', async (interaction) => {
       iconURL: 'https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3',
     });
 
-    interaction.reply({ embeds: [embed] });
+    interaction.reply({ embeds: [joke_embd] });
   }
 });
 
