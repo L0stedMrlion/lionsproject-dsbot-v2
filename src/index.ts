16db0cf78a648a6fee7c7a4cece0445d873c0ed7
Mrlion <contact@lionsproject.eu>
2023-11-11 22:05:52 +0000

    added files

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { Client } from 'discord.js';
+
+const client = new Client({
+    intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent'],
+});
+
+try {
+    client.login(process.env.TOKEN);
+  } catch (error) {
+    console.error(" Lion's Project™ - Unable to login to Discord ");
+  }
