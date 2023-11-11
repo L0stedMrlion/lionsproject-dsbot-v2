2dfa46617f9ed1e1e777afceda8ab74e7b923d09
Mrlion <contact@lionsproject.eu>
2023-11-11 09:09:52 +0000

    edited console.log too

--- src/index.js
+++ src/index.js
@@ -237,7 +237,7 @@ client.on('interactionCreate', async (interaction) => {
         iconURL: 'https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3',
       });
 
-    interaction.reply({ content: ":envelope: Message was send to user!", ephemeral: false });
+    interaction.reply({ content: ":envelope: Message was send to user!", ephemeral: true });
     user.send({ embeds: [auth_embd] });
   }
 });
