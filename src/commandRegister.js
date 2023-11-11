81b45888b0bf6b33eb98bcc352b8363f50ccaae5
Mrlion <contact@lionsproject.eu>
2023-11-11 09:09:39 +0000

    edited console.log

--- src/commandRegister.js
+++ src/commandRegister.js
@@ -43,19 +43,19 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 (async () => {
     try {
         console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-        console.log(" Lion’s Project™ - Discord bot - ", process.env.BOT_VERSION);
+        console.log("  Lion’s Project™ - Discord bot - ", process.env.BOT_VERSION);
         console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-        console.log(' Lion’s Project™ - Checking and registering new slash commands... ');
+        console.log('  Lion’s Project™ - Checking and registering new slash commands... ');
 
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands }
         );
 
-        console.log(' Lion’s Project™ - Slash commands were registered successfully!');
+        console.log('  Lion’s Project™ - Slash commands were registered successfully!  ');
     } catch (error) {
         console.error('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-        console.error(` Lion’s Project™ - There was an error: ${error}`);
+        console.error(`  Lion’s Project™ - There was an error: ${error}`);
         console.error('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
     }
 })();
