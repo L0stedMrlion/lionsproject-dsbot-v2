c0aa4896f471ce5c0c706ef3838903bd3bf00a74
Mrlion <contact@lionsproject.eu>
2023-11-12 13:57:10 +0000

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const { Client } = require("discord.js")
+const { Client, GatewayIntentBits } = require("discord.js")
 const dotenv = require('dotenv');
 dotenv.config();
+
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.GuildScheduledEvents,
+      GatewayIntentBits.DirectMessages,
+    ],
+  });
+
+client.login(process.env.TOKEN);
