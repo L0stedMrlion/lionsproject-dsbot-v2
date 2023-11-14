4017c074689f85af0817eec81ddf5ed1b82fd134
Mrlion <contact@lionsproject.eu>
2023-11-14 15:54:21 +0000

    added new files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const { Client, GatewayIntentBits } = require("discord.js")
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.GuildMessageReactions,
-      GatewayIntentBits.GuildScheduledEvents,
-      GatewayIntentBits.DirectMessages,
-    ],
-  });
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+eventHandler(client);
 
 client.login(process.env.TOKEN);
