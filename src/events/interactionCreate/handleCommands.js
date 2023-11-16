364556b441642578dab615f663f20a77a6dd24af
Mrlion <contact@lionsproject.eu>
2023-11-16 16:13:14 +0000

    added and better spell check

--- src/events/interactionCreate/handleCommands.js
+++ src/events/interactionCreate/handleCommands.js
@@ -16,17 +16,7 @@ module.exports = async (client, interaction) => {
     if (commandObject.devOnly) {
       if (!devs.includes(interaction.member.id)) {
         interaction.reply({
-          content: 'Only developers are allowed to run this command.',
-          ephemeral: true,
-        });
-        return;
-      }
-    }
-
-    if (commandObject.testOnly) {
-      if (!(interaction.guild.id === 1003239269994876938)) {
-        interaction.reply({
-          content: 'This command cannot be ran here.',
+          content: '🛠️ Only developers are allowed to run this command!',
           ephemeral: true,
         });
         return;
@@ -37,7 +27,7 @@ module.exports = async (client, interaction) => {
       for (const permission of commandObject.permissionsRequired) {
         if (!interaction.member.permissions.has(permission)) {
           interaction.reply({
-            content: 'Not enough permissions.',
+            content: '❌ Not enough permissions.',
             ephemeral: true,
           });
           return;
