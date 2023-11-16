b9ee497fbec73df293852a91a45f1dbfba6f3abe
Mrlion <contact@lionsproject.eu>
2023-11-16 14:45:42 +0000

    some changes

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
-  console.log(`${client.user.tag} starting bot...`)
+  console.log(`${client.user.tag} starting bot...`);
   console.log(`${client.user.tag} is online.`);
 };
