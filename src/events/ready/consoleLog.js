b3681cf7460291045bc9e0027005a5bf144dcfb2
Mrlion <contact@lionsproject.eu>
2023-11-16 16:45:12 +0000

    again edited console.log on startup

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = (client) => {
-  console.log(`${client.user.tag} - `, process.env.BOT_VERSION)
-  console.log(`${client.user.tag} starting bot...`);
-  console.log(`${client.user.tag} is online.`);
+  console.log(`🦁 LionBot -`, process.env.BOT_VERSION)
+  console.log(`🦁 LionBot starting bot...`);
+  console.log(`🦁 LionBot is online.`);
 };
