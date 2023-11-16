db29e5cbfd06d987157cc2f1bd03f98b4a5ace0c
Mrlion <contact@lionsproject.eu>
2023-11-16 16:27:30 +0000

    added command /purge

---
+++ src/commands/misc/purge.js
@@ -0,0 +1,9 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: 'purge',
+    description: '🧹 Purge all command not older than 14 days!',
+
+    callback: async (client, interaction) => {
+    },
+};
