b655ad523946ebadd1bb576e9cdbc07445a8a995
Mrlion <contact@lionsproject.eu>
2023-11-16 16:44:32 +0000

    added version to console.log

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 module.exports = (client) => {
+  console.log(`${client.user.tag} - `, process.env.BOT_VERSION)
   console.log(`${client.user.tag} starting bot...`);
   console.log(`${client.user.tag} is online.`);
 };
