ecff99cf082ef3602dd9f80c53c5de8679f971d2
Mrlion <contact@lionsproject.eu>
2023-11-16 14:42:32 +0000

    added color to status_embed

--- src/commands/misc/status.js
+++ src/commands/misc/status.js
@@ -10,6 +10,7 @@ module.exports = {
         const status_embed = new EmbedBuilder()
           .setTitle('📊 Status page')
           .setDescription("All statuses you can find here on your [Status page](https://status.lionsproject.eu)")
+          .setColor('#64a338')
           .setFooter({
             text: "🦁 Lion's Project™ © 2023",
             iconURL: 'https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3',
