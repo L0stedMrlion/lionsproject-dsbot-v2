c4d4a55e97fe9d951ea8a3b2e5ee13c36072c908
Mrlion <contact@lionsproject.eu>
2023-11-16 16:29:47 +0000

    spell error in description

--- src/commands/misc/purge.js
+++ src/commands/misc/purge.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
     name: 'purge',
-    description: '🧹 Purge all command not older than 14 days!',
+    description: '🧹 Purges all command not older than 14 days!',
 
     callback: async (client, interaction) => {
     },
