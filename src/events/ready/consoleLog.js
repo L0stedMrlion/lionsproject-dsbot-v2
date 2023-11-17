824cd9e5be0412fddfcdf66853286ab307b7dc2b
Mrlion <contact@lionsproject.eu>
2023-11-17 07:42:03 +0000

    added presence status for the bot.

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
+const { ActivityType } = require('discord.js');
 
 module.exports = (client) => {
   console.log(`🦁 LionBot -`, process.env.BOT_VERSION)
   console.log(`🦁 LionBot starting bot...`);
   console.log(`🦁 LionBot is online.`);
+  client.user.setPresence({
+    activities: [{ name: `🦁 Lion's Project™`, type: ActivityType.Listening }],
+    status: 'online',
+  });
 };
