1f3b0c0fb445134cdefc20745b301c93036c45fe
Mrlion <contact@lionsproject.eu>
2023-11-17 17:18:31 +0000

    removed from purge.js imported embedbuilder from ds.js

--- src/commands/misc/purge.js
+++ src/commands/misc/purge.js
@@ -1,9 +1,8 @@
-const { EmbedBuilder } = require("discord.js");
-
 module.exports = {
     name: 'purge',
     description: '🧹 Purges all command not older than 14 days!',
 
     callback: async (client, interaction) => {
+
     },
 };
