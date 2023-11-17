19679b14d192a9ee6432bfe0c04aa4af710c1ae2
Mrlion <contact@lionsproject.eu>
2023-11-17 17:23:17 +0000

    now working, still some bugs, will be fixed soon

--- src/commands/misc/purge.js
+++ src/commands/misc/purge.js
@@ -1,22 +1,34 @@
 module.exports = {
     name: 'purge',
-    description: '🧹 Purges all command not older than 14 days!',
-
+    description: '🧹 Purges a specified number of messages.',
+  
     callback: async (client, interaction) => {
-        if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
-            return interaction.reply('❌ You do not have the required permissions to use this command.');
-          }
-      
-          const messages = await interaction.channel.messages.fetch({ limit: 200 });
-      
-          const filteredMessages = messages.filter(message => Date.now() - message.createdTimestamp <= 1209600000);
-      
-          try {
-            await interaction.channel.bulkDelete(filteredMessages);
-            interaction.reply(`🧹 Successfully purged ${filteredMessages.length} messages.`);
-          } catch (error) {
-            console.error('Error purging messages:', error);
-            interaction.reply('❌ An error occurred while purging messages.');
-          }
-        },
-      };
+      if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+        return interaction.reply('❌ You do not have the required permissions to use this command.');
+      }
+  
+      const count = parseInt(interaction.options.getInteger('count'));
+  
+      if (!count || count < 1 || count > 100) {
+        return interaction.reply('❌ Please enter a valid message count between 1 and 100.');
+      }
+  
+      const messages = await interaction.channel.messages.fetch({ limit: 100 });
+  
+      const filteredMessages = messages.filter(message => Date.now() - message.createdTimestamp <= 1209600000); // 14 days in milliseconds
+  
+      if (filteredMessages.length < count) {
+        return interaction.reply('❌ Not enough messages found to purge the specified count.');
+      }
+  
+      const messagesToDelete = filteredMessages.slice(0, count);
+  
+      try {
+        await interaction.channel.bulkDelete(messagesToDelete);
+        interaction.reply(`🧹 Successfully purged ${count} messages.`);
+      } catch (error) {
+        console.error('Error purging messages:', error);
+        interaction.reply('❌ An error occurred while purging messages.');
+      }
+    },
+  };
