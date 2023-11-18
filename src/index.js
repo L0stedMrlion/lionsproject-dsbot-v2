a39554b13418f2653b43d9ab16e90acfb1adece8
Mrlion <contact@lionsproject.eu>
2023-11-18 14:35:32 +0000

    delete some intents

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.GuildPresences,
   ],
 });
 
