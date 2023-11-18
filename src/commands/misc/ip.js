1e63444f6ae92871c82cd799891044766ef8449a
Mrlion <contact@lionsproject.eu>
2023-11-18 08:11:20 +0000

    added working /ip command with placeholder

---
+++ src/commands/misc/ip.js
@@ -0,0 +1,20 @@
+const config = require("../../../config.json");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "ip",
+  description: "😁 Send you IP adress for our Minecraft server!",
+
+  callback: async (client, interaction) => {
+    const ip_embd = new EmbedBuilder()
+      .setTitle("🦁 LionBot")
+      .setDescription(`IP adress for our server is **${config.minecraft}**`)
+      .setColor("#00AA00")
+      .setFooter({
+        text: "🦁 Lion's Project™ © 2023",
+        iconURL: "https://cdn.discordapp.com/attachments/1092013099168583781/1170441421900218448/lionsproject_logo.png?ex=65590d84&is=65469884&hm=e321b014c27e21524e8efe2b72823971d9dfe8ffff1fedcced5b65391c4816b3",
+      });
+
+    interaction.reply({ embeds: [ip_embd] });
+  },
+};
